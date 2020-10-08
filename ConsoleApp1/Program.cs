56b2000a56c4fb0ef886765e19c0007839a59dbd
Коля Васильев <nikolayvas20@gmail.com>
2020-10-08 23:04:50 +0300

    52

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine( "Введите чесло А");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите чесло В");
+                int b = int.Parse(Console.ReadLine());
+                Console.WriteLine(a * a + b * b + " сума квадратов");
+                Console.WriteLine(a * a + 2 * a * b + b * b + " Квадрат сумы");
+                if (a * a + b * b == a * a + 2 * a * b + b * b) Console.WriteLine("они равны");
+                else if (a * a + b * b > a * a + 2 * a * b + b * b) Console.WriteLine("сума квадратов больше");
+                else Console.WriteLine("Квадрат сумы больше");
+                
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
         }
     }
 }
